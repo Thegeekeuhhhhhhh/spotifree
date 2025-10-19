37e43a310486bcf2d2c61c9dc804d6d520b65dfa
Leo Menaldo <leo.menaldo@epita.fr>
2025-10-19 16:08:56 +0200

    Fixed things for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ relative paths instead of absolute
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+  base: '/spotifree',
 })
